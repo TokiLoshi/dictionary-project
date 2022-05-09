82d26c91323a33a2aee599f6bd99f820ad6dc7eb
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2022-05-09 11:59:20 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header m-4">
+        Hello World
+        
+      </header>
+      <button className='btn btn-primary'>Hello</button>
+    </div>
+  );
+}
+
+export default App;
