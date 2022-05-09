b24b23f7708265b03dafa56a9f8d60eb715f1225
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2022-05-09 14:38:37 -0700

    rendered word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary(event) {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
 function handleResponse(response) {
   console.log(response.data[0]);
+  console.log(response.data[0].meanings[0].definitions[0].definition);
+  setResults(response.data[0]);
 }
   function search(event) {
   event.preventDefault();
@@ -26,7 +30,7 @@ function handleKeywordChange(event) {
       <input type="search" placeholder="search for a word" autoFocus={true} onChange={handleKeywordChange}/>
       <input type="submit" value="Search" className="Search btn btn-primary m-2" />
       </form>
-<p>You are searching for:...{keyword} </p>
+<Results results={results}/>
     </div>
   )
 }
