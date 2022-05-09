6bc67ebc500198d279f42f9998950d1d078b7a2f
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2022-05-09 14:55:05 -0700

    added css and footer links

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
+    <div className="App m-2">
+      <div className="container shadow">
       <header className="App-header m-4">
         <h1>Dictionary App</h1>
         
       </header>
       <Dictionary />
-    <footer><p className='App-footer'><small>Open sourced, coded by Bianca Silva</small></p></footer>
+    <footer><p className='App-footer'><small><a href='https://github.com/TokiLoshi/dictionary-project' target='blank' rel='noopener noreferrer'>Open sourced</a>, coded by <a href='https://tiny-valkyrie-cad23c.netlify.app/' target='blank' rel='noopener noreferrer'>Bianca Silva</a></small></p></footer>
     </div>
     </div>
   );
