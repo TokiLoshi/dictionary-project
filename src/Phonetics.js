24a8738311270d17a013021f850109bd291a61af
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2022-05-10 13:51:20 -0700

    Updated icon for phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics(props) {
   function playAudio(){
@@ -11,7 +12,8 @@ if (props.phonetic.audio) {
   return (
     <div>
       <h4>{props.phonetic.text}</h4>
-      <button className="phoneticButton btn btn-dark p-2 m-3" aria-label="listen" onClick={playAudio} >Click to listen</button>
+      {/* <button className="phoneticButton btn btn-dark p-2 m-3" aria-label="listen" onClick={playAudio} >Click to listen</button> */}
+      <i className="fa-solid fa-circle-play m-2 p-4" aria-label="listen" onClick={playAudio}></i>
     </div>
   );
 } else {
